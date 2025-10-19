32737a27db842c15ad5fff757e895fffafe4c0dc
s619 <squarix@live.fr>
2025-10-19 22:11:06 +0100

    fixed DLL template

--- linked_lists/DLList.h
+++ linked_lists/DLList.h
@@ -10,8 +10,8 @@ class DLLNode{
   public:
     T data; 
     DLLNode *next, *prev;
-    DLLNode():next(nullptr),prev(nullptr){};
-    DLLNode(const T& elt, DLLNode *p=nullptr, DLLNode *q=nullptr):data(elt),next(p),prev(q){};
+    DLLNode<T>():next(nullptr),prev(nullptr){};
+    DLLNode<T>(const T& elt, DLLNode *p=nullptr, DLLNode *q=nullptr):data(elt),next(p),prev(q){};
 };
 
 template<class T>
@@ -20,9 +20,46 @@ class DLList{
     DLList():head(nullptr),tail(nullptr){};
     void addToTail(const T&);
     T deleteFromTail();
+    T getTail();
+    T getHead();
+    void setTail(const T&);
+    void setHead(const T&);
   protected:
     DLLNode<T> *head, *tail;
 };
+
+template<class T>
+void DLList<T>::addToTail(const T& a){
+  if (tail){
+    tail = new DLLNode<T>(a, nullptr, tail);
+    tail->prev->next=tail;
+  }
+  else
+    head=tail=new DLLNode<T>(a);
+}
+
+template<class T>
+T DLList<T>::deleteFromTail(){
+  T x=tail->data;
+  if (head==tail){
+    delete head;
+    head=tail=nullptr;
+  }
+  else{
+    tail=tail->prev;
+    delete tail->next;
+    tail->next=nullptr;
+  }
+  return x;
+}
+
+
+template<class T>
+T DLList<T>::getHead(){
+  T x=head->data;
+  return x;
+}
+
 
 #endif
 
