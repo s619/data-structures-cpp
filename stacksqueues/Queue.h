dbff5a2782f87d2080099a2c52927d71b06769bd
s619 <squarix@live.fr>
2025-10-24 11:49:11 +0100

    dll based queue

---
+++ stacksqueues/Queue.h
@@ -0,0 +1,37 @@
+//********************************** Queue.h *************************************
+//                    generic doubly linked list based queue
+
+
+#ifndef DLL_QUEUE
+#define DLL_QUEUE
+
+#include <list>
+
+
+template<class T>
+class Queue{
+  private:
+    list<T> lst;
+
+  public:
+    Queue(){}
+    void clear(){
+      lst.clear();
+    }
+    bool isEmpty() const {return lst.empty();}
+    T& front(){return lst.front();}
+    void enqueue(const T&);
+    T dequeue();
+};
+
+template<class T>
+void Queue<T>::enqueue(const T& el){
+  lst.push_back(el);
+}
+
+template<class T>
+T Queue<T>::dequeue(){
+  T ret= lst.front();
+  lst.pop_front();
+  return ret;
+}
